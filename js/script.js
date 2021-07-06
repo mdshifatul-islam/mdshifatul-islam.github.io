077a5c21232b157930bccd4a04e9e26310da40a9
mdshifatul-islam <86969168+mdshifatul-islam@users.noreply.github.com>
2021-07-06 22:57:52 +0600

    Upload Site

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function(){
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 20,
+          stretch: 0,
+          depth: 300,
+          modifier: 1,
+          slideShadows: true,
+        },
+        loop: true,
+        // autoplay: {
+        //     delay: 7000,
+        //     disableOnInteraction: false,
+        //   },
+      });
+})
